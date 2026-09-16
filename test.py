3bc99bdbf72dc899b4e569e38b925a99138c4609
Mohammadaw12 <awlukamodri@gmail.com>
2026-09-16 09:38:03 +0200

    ajout test_2

--- test.py
+++ test.py
@@ -5,3 +5,9 @@ def test_1():
     assert f.puissance(2, 3) == 8
     assert f.puissance(2, 2) == 4
 
+def test_2():
+    assert f.puissance(-1, 2) == 1
+    assert f.puissance(-1, 3) == -1
+    assert f.puissance(-1, -1) == -1
+    assert f.puissance(-1, -2) == 1
+    assert f.puissance(-2, -1) == -0.5
