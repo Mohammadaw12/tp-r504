153e57afc62c6751a9abf51a585ab5c24dfd3c48
Mohammadaw12 <awlukamodri@gmail.com>
2026-09-16 11:16:45 +0200

    Ajout des test cas limites

--- TP1/puissance.py
+++ TP1/puissance.py
@@ -5,5 +5,9 @@ def puissance(a, b):
     if not type(b) is int:
         raise TypeError("Only integers are allowed")
 
+    if a == 0 and b < 0:
+        raise ValueError("0 cannot be raised to a negative power")
+
     return a ** b
+
 
